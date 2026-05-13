aaeebd61a7c2fbc87d9103139796d5f78e3f3ca6
Francisco Reyes <francisco.reyes.developer@email.com>
2026-05-13 17:06:26 +0200

    A

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  integrations: [tailwind(), sitemap()],
-  site: 'https://hospitaljesusnazareno.es',
-});
