a01194af1ffddff7602547d4f506620e83a9b79d
reyesfr24 <franciscoreyesluna@gmail.com>
2026-05-18 20:21:20 +0200

    Estructura base

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Historia from './components/Historia';
-import Servicios from './components/Servicios';
-import Galeria from './components/Galeria';
-import Contacto from './components/Contacto';
-import Footer from './components/Footer';
-
 export default function App() {
   return (
-    <div className="font-sans">
-      <Navbar />
-      <main>
-        <Hero />
-        <Historia />
-        <Servicios />
-        <Galeria />
-        <Contacto />
-      </main>
-      <Footer />
+    <div>
+      <h1>Hospital de Jesús Nazareno</h1>
     </div>
   );
 }
